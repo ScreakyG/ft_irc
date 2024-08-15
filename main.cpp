80fab342b4f06c62440c1aaebef0fb7b5b800a36
Parallels <francois.gnz@gmail.com>
2024-08-15 22:52:56 +0200

    added header

--- main.cpp
+++ main.cpp
@@ -2,13 +2,26 @@
 #include "Server.hpp"
 #include "Random.hpp"
 
+#include <stdio.h>
+
+const char *getClientHeader(void)
+{
+    const char *header = "---------------------------\n---WELCOME TO THE SERVER---\n---------------------------\n";
+
+    return (header);
+}
+
 int main(void)
 {
     Server server;
+    std::cout << getClientHeader() << std::endl;
+
     try
     {
         server.createServerSocket();
         server.listenPort();
+
+        // Definier si on utilise select(), poll(), epoll() ou kqueue();
     }
     catch (const std::exception &e)
     {
