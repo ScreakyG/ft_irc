43783d90a9e4961f95bda05748b6fe8e9c7af7b1
Parallels <francois.gnz@gmail.com>
2024-09-12 19:36:26 +0200

    changed PASS behaviour to not disconnect a client on a badpassword

--- commands/pass.cpp
+++ commands/pass.cpp
@@ -29,12 +29,14 @@ void   exec_PASS(Server &server, std::vector<std::string> &arguments, int client
 
     if (server.getServerPassword() != arguments[0])
     {
-        message = ERR_PASSWDMISMATCH(client->getNickname());
-        server.sendToClient(message, clientFd);
-        message = ERROR_MSG(std::string("Closing Link: localhost (Bad Password)"));  //Cette partie provoque un segfault si plusieurs mot de passes loupes d'affiles
+        client->setServerPassword(false);
+        message = ERR_PASSWDMISMATCH(client->getNickname()); // Pas utiliser normanelement pour ce cas mais ici cela permet de quand meme signaler un mauvais password.
         server.sendToClient(message, clientFd);
 
-        throw Server::ClientDisconnect();
+        //message = ERROR_MSG(std::string("Closing Link: localhost (Bad Password)"));  //Cette partie provoque un segfault si plusieurs mot de passes loupes d'affiles.
+        //server.sendToClient(message, clientFd);
+
+        //throw Server::ClientDisconnect(); // On peut changer le comportement si on souhaite deconnecter un client lors d' un mauvais mot de passe.
     }
 
     if (server.getServerPassword() == arguments[0])
