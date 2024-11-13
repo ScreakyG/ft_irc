dab7caee51cb97b8c043a5644eda832269fa0b18
ketzon <bessonflorian42@yahoo.fr>
2024-11-13 13:44:34 +0100

    no text to send

--- commands/invite.d
+++
@@ -1,4 +0,0 @@
-commands/invite.o: commands/invite.cpp commands/../includes/Commands.hpp \
- commands/../includes/Server.hpp commands/../includes/Random.hpp \
- commands/../includes/Client.hpp commands/../includes/Channel.hpp \
- commands/../includes/Responses.hpp commands/../includes/Colors.hpp
--- commands/pass.d
+++
@@ -1,4 +0,0 @@
-commands/pass.o: commands/pass.cpp commands/../includes/Commands.hpp \
- commands/../includes/Server.hpp commands/../includes/Random.hpp \
- commands/../includes/Client.hpp commands/../includes/Channel.hpp \
- commands/../includes/Responses.hpp commands/../includes/Colors.hpp
